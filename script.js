a126b4e5d4354011a4f30186f6ef5efc4049f61b
Tristan <tristan_wilson_63@yahoo.com>
2022-09-01 21:39:53 -0500

    calling api and displaying info

--- script.js
+++ script.js
@@ -1,22 +1,46 @@
 const searchEL = document.getElementById("searchText");
 const submitEl = document.getElementById("submit");
+const userFormEl = document.getElementById("form1")
 
-let cityEl = searchEL.value;
-submitEl.addEventListener("click", function() {
-    cityEl = searchEL.value;
-    console.log(cityEl)
-  })
+var formSubmit = function (event) {
+  event.preventDefault();
+
+  var city = searchEL.value.trim();
+
+  if (city) {
+    getInputText(city);
+
+    searchEL.value = '';
+  }
+};
+
+var getInputText = function (city) {
+  var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=42bd4df4c8216e16be280cf95790436b';
+
+  fetch(apiUrl)
+  .then(function (response) {
+    if (response.ok) {
+      console.log(response);
+      response.json().then(function (data) {
+        var icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+var temp = Math.floor(data.main.temp) + "°F";
+ var humidity = data.main.humidity + "%";
+ var wind = data.wind.speed;
+ document.getElementById("searchText").value;
 
 
-$.getJSON("https://api.openweathermap.org/data/2.5/weather?q=" + cityEl + "&units=imperial&appid=42bd4df4c8216e16be280cf95790436b", function(data){
 
-var icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
-var temp = Math.floor(data.main.temp);
-var weather = data.weather[0].main;
-document.getElementById("searchText").value;
+ $('.icon').attr('src', icon);
+ $('.temp').empty().append(temp);
+ $('.humidity').empty().append(humidity);
+$('.city').empty().append(city);
+$('.wind').empty().append(wind);
+      });
+    }
+  });
+};
 
-$('.icon').attr('src', icon);
-$('.temp').append(temp);
-$('.weather').append(weather);
+userFormEl.addEventListener('submit', formSubmit);
 
-})
+var gradDate = moment().subtract(10, 'days').calendar();;
+$("#1a").text(gradDate);
