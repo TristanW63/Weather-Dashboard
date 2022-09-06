1a658cd483634bba44f3a7076bb2e6c6ebb72ca9
Tristan <tristan_wilson_63@yahoo.com>
2022-09-05 19:37:19 -0500

    currently pulling my hair out

--- script.js
+++ script.js
@@ -1,42 +1,49 @@
 const searchEL = document.getElementById("searchText");
 const submitEl = document.getElementById("submit");
 const userFormEl = document.getElementById("form1")
+var skillsListEl = $('#skills-list');
+
+var savedSearches = function (searched) {
+  var listEl = $('<li>');
+  var listDetail = searched.concat('');
+  listEl.addClass('list-group-item').text(listDetail);
+  listEl.appendTo(skillsListEl);
+};
+
+
+
 
 
 var formSubmit = function (event) {
   event.preventDefault();
 
   var city = searchEL.value.trim();
-  var state = searchEL.value.trim();
-  var cityState = city + state;
+
   if (city) {
     getInputText(city);
 
     searchEL.value = '';
-
-  } else if (state) {
-    getInputText(state);
-
-    searchEL.value = '';
+  } if (!city) {
+    console.log('You need to fill out the form!');
+    return;
   }
+  savedSearches(city);
 };
 
-
-const API = `42bd4df4c8216e16be280cf95790436b`;
 var getInputText = function (city) {
-  var apiUrl = `api.openweathermap.org/data/2.5/forecast?q=${city},${state}&appid=${API}`;
+  var apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&units=imperial&appid=42bd4df4c8216e16be280cf95790436b&cnt=40';
 
   fetch(apiUrl)
-  .then(function (response) {
-    if (response.ok) {
-      console.log(response);
-      response.json().then(function (data) {
-        var icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
-var temp = Math.floor(data.main.temp) + "°F";
- var humidity = data.main.humidity + "%";
- var wind = data.wind.speed;
- document.getElementById("searchText").value;
+  .then(response => response.json()) 
+    .then(forecast => {
+      forecast.list.forEach(day => {
 
+        var icon = "http://openweathermap.org/img/w/" + day.weather[0].icon + ".png";
+var temp = Math.floor(day.main.temp) + "°F";
+ var humidity = day.main.humidity + "%";
+ var wind = day.wind.speed;
+ document.getElementById("searchText").value;
+console.log(forecast.length)
 
 
  $('.icon').attr('src', icon);
@@ -44,10 +51,10 @@ var temp = Math.floor(data.main.temp) + "°F";
  $('.humidity').empty().append(humidity);
 $('.city').empty().append(city);
 $('.wind').empty().append(wind);
-      });
-    }
-  });
-};
+        })
+    })
+
+      }
 
 userFormEl.addEventListener('submit', formSubmit);
 
